4991453d2440294e949e6adddc31f8e6d7c9a87b
MatiasStorm <48760011+MatiasStorm@users.noreply.github.com>
2019-03-20 17:29:38 +0100

    Allowed moves for all pieces added to ChessEngine.py

--- ChessEngine.py
+++ ChessEngine.py
@@ -1,12 +1,9 @@
-
-
-
-
 class ChessEngine():
     def __init__(self, white_pos = "BUTTOM", black_pos = "TOP"):
         self.white_pos = white_pos
         self.black_pos = black_pos
-        self.get_moves_dict = {"P":self.__get_pawn_moves}
+        self.get_moves_dict = {"P":self.__get_pawn_moves, "H":self.__get_knight_moves, "B":self.__get_bishop_moves,
+                               "R":self.__get_rock_moves, "Q":self.__get_queen_moves, "K":self.__get_king_moves}
 
     def __get_pawn_moves(self, board, pos, color):
         moves = []
@@ -14,21 +11,86 @@ class ChessEngine():
             if pos[0] - 1 >= 0:
                 if board[pos[0] - 1][pos[1]] == "":
                     moves.append((pos[0] - 1, pos[1]))
-            if self.white_pos == "BUTTOM" and pos[0] == 6 and board[pos[0] - 2][pos[1]] == "":
+            if self.white_pos == "BUTTOM" and pos[0] == 6 and board[pos[0] - 2][pos[1]] == "": # If pawn is at start position it can move to places.
                 moves.append((pos[0] - 2, pos[1]))
         
         elif color == "b":
             if pos[0] + 1 <= 7:
                 if board[pos[0] + 1][pos[1]] == "":
                     moves.append((pos[0] + 1, pos[1]))
-            if self.black_pos == "TOP" and pos[0] == 1 and board[pos[0] + 2][pos[1]] == "":
+            if self.black_pos == "TOP" and pos[0] == 1 and board[pos[0] + 2][pos[1]] == "": 
                 moves.append((pos[0] + 2, pos[1]))
         return moves
     
+    def __get_knight_moves(self, board, pos, color):
+        steps =[[1, 2], [-1, 2], [2, -1], [2, 1], [1, -2], [-1, -2], [-2, 1], [-2, -1]]
+        moves = []
+        for step in steps:
+            row = pos[0] + step[0]
+            col = pos[1] + step[1]
+            if 0 <= row <= 7 and 0 <= col <= 7 and color not in board[row][col]:
+                moves.append((row, col))
+        return moves
+    
+    def __get_directed_moves(self, board, pos, color, row_change, col_change):
+        """
+        Return all allowed moves in the direction specified by row_change and col_change, from pos.
+        The first occurence of a piece of a different color on the directed path is also added to the allowed moves.
+        """
+        moves = []
+        row, col = pos
+        row += row_change
+        col += col_change
+        if 7 < row or row < 0 or col < 0 or col > 7:
+            return moves
+        while 0 <= row <= 7 and 0 <= col <= 7 and board[row][col] == "":
+            moves.append((row, col))
+            row += row_change
+            col += col_change
+        if 7 < row or row < 0 or col < 0 or col > 7:
+            return moves
+            
+        if board[row][col][0] != color:
+            moves.append((row, col))
+        return moves
+        
+    def __get_bishop_moves(self, board, pos, color):
+        moves = []
+        moves += self.__get_directed_moves(board, pos, color, -1, -1) # NE
+        moves += self.__get_directed_moves(board, pos, color, -1, 1) # NW
+        moves += self.__get_directed_moves(board, pos, color, 1, -1) # SE
+        moves += self.__get_directed_moves(board, pos, color, 1, 1) # SW
+        return moves
+    
+    def __get_rock_moves(self, board, pos, color):
+        moves = []
+        moves += self.__get_directed_moves(board, pos, color, -1, 0) # N
+        moves += self.__get_directed_moves(board, pos, color, 1, 0) # S
+        moves += self.__get_directed_moves(board, pos, color, 0, -1) # E
+        moves += self.__get_directed_moves(board, pos, color, 0, 1) # W
+        return moves
+    
+    def __get_queen_moves(self, board, pos, color):
+        moves = []
+        moves += self.__get_rock_moves(board, pos, color) # All rock-like moves
+        moves += self.__get_bishop_moves(board, pos, color) # All bishop-like moves
+        return moves
+    
+    def __get_king_moves(self, board, pos, color):
+        moves = []
+        for r in range(-1, 2):
+            for c in range(-1, 2):
+                row = pos[0] + r
+                col = pos[1] + c
+                if (row, col) != pos and 0 <= row <= 7 and 0 <= col <= 7:
+                    if board[row][col] == "":
+                        moves.append((row, col))
+                    elif board[row][col][0] != color:
+                        moves.append((row, col))
+        return moves
+    
     def is_move_allowed(self, board, start_pos, end_pos):
-        piece_str = board[start_pos[0]][start_pos[1]]
-        color = piece_str[0]
-        piece = piece_str[1]
+        return end_pos in self.get_allowed_moves(board, start_pos)
         
 
     def get_allowed_moves(self, board, pos):
@@ -38,23 +100,18 @@ class ChessEngine():
     
     def game_over(self, board):
         pass
-    
-    def p(self, t):
-        print(t)
 
-
-b = [["bR", "bH", "bB", "bQ", "bK", "bB", "bH", "bR"],
-     ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
-     ["", "", "", "", "", "", "", ""],
-     ["", "", "", "", "", "", "", ""],
-     ["", "", "", "", "", "", "", ""],
-     ["", "", "", "", "", "", "", ""],
-     ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
-     ["wR", "wH", "wB", "wQ", "wK", "wB", "wH", "wR"]]
-                     
-e = ChessEngine()
-pos = (2, 1)
-print(e.get_allowed_moves(b, pos))  
+# b = [["bR", "bH", "bB", "bQ", "bK", "bB", "bH", "bR"],
+#      ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
+#      ["", "", "", "", "", "", "", ""],
+#      ["", "", "", "", "bB", "", "", ""],
+#      ["", "", "", "", "", "", "", ""],
+#      ["", "", "", "", "", "", "", ""],
+#      ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
+#      ["wR", "wH", "wB", "wQ", "wK", "wB", "wH", "wR"]]
+# e = ChessEngine()
+# e.get_allowed_moves(b, (3, 4))
+ 
 
 
 
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from Settings import *
 from Board import Board
 from math import floor
+from ChessEngine import ChessEngine
 
 class Game():
     def __init__(self):
@@ -11,6 +12,7 @@ class Game():
         self.clock = pg.time.Clock()
         pg.display.set_caption(TITLE)
         self.load_data()
+        self.engine = ChessEngine()
         
     def load_data(self):
         self.image_dict = {}
@@ -58,14 +60,14 @@ class Game():
                         self.start_pos = self.get_mouse_pos_rc()
                         if "w" not in self.piece_matrix[self.start_pos[0]][self.start_pos[1]]:
                             self.start_pos = ()
-                        
             elif event.type == pg.MOUSEBUTTONDOWN and event.button == RIGHT:
                 self.start_pos = ()
 
     def move_piece(self, start_pos, end_pos):
-        moving_piece = self.piece_matrix[start_pos[0]][start_pos[1]]
-        self.piece_matrix[start_pos[0]][start_pos[1]] = ""
-        self.piece_matrix[end_pos[0]][end_pos[1]] = moving_piece
+        if self.engine.is_move_allowed(self.piece_matrix, start_pos, end_pos): # Only move piece if the move is allowed
+            moving_piece = self.piece_matrix[start_pos[0]][start_pos[1]]
+            self.piece_matrix[start_pos[0]][start_pos[1]] = ""
+            self.piece_matrix[end_pos[0]][end_pos[1]] = moving_piece
         
     def draw(self):
         self.board.draw()
@@ -80,15 +82,26 @@ class Game():
                     x = MARGIN + TILESIZE * col_index
                     self.screen.blit(image, (x, y))
         
-        if self.start_pos: ## if start position is defined, mark it
+        if self.start_pos: ## if start position is defined, mark it and show allowed moves
             mark_surface = pg.Surface((TILESIZE, TILESIZE))
             mark_surface.set_alpha(175)
             mark_surface.fill(BLACK)
             y = MARGIN + TILESIZE * self.start_pos[0]
             x = MARGIN + TILESIZE * self.start_pos[1]
             self.screen.blit(mark_surface, (x, y))
+            self.show_allowed_moves()
             
         pg.display.update()
+        
+    def show_allowed_moves(self):
+        moves = self.engine.get_allowed_moves(self.piece_matrix, self.start_pos)
+        for move in moves:
+            mark_surface = pg.Surface((TILESIZE, TILESIZE))
+            mark_surface.set_alpha(100)
+            mark_surface.fill(GREEN)
+            y = MARGIN + TILESIZE * move[0]
+            x = MARGIN + TILESIZE * move[1]
+            self.screen.blit(mark_surface, (x, y))
 
 if __name__ == "__main__":
     game = Game()

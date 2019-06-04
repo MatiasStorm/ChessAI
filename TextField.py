ef8ee8db6c13dccfbd2c8ad8c0c9488037b05511
MatiasStorm <48760011+MatiasStorm@users.noreply.github.com>
2019-06-04 16:57:22 +0200

    Create TextField.py

---
+++ TextField.py
@@ -0,0 +1,21 @@
+import pygame as pg
+from Settings import *
+
+class TextField():
+    def __init__(self):
+        self.surface = pg.Surface((TF_WIDTH - MARGIN, TF_HEIGHT - MARGIN * 2))
+        self.font_size = 15
+        self.y = 0
+        self.font = pg.font.SysFont("consolas", self.font_size)
+    
+    def clear(self):
+        self.surface = pg.Surface((TF_WIDTH - MARGIN, TF_HEIGHT - MARGIN * 2))
+        self.y = 0
+    
+    def write(self, text, color=WHITE):
+        if self.y >= TF_HEIGHT - self.font_size*4:
+            self.clear()
+        text = self.font.render(text, True, color)
+        self.surface.blit(text, (0,self.y))
+        self.y += self.font_size
+        
